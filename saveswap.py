e65f2c527da9b4d21bbb1afd8cda3f04c255c932
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2017-08-27 00:15:12 -0400

    Initial commit: My project boilerplate

---
+++ saveswap.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""[application description here]"""
+
+from __future__ import (absolute_import, division, print_function,
+                        with_statement, unicode_literals)
+
+__author__ = "Stephan Sokolow (deitarion/SSokolow)"
+__appname__ = "[application name here]"
+__version__ = "0.0pre0"
+__license__ = "GNU GPL 3.0 or later"
+
+import logging
+log = logging.getLogger(__name__)
+
+# -- Code Here --
+
+def main():
+    """The main entry point, compatible with setuptools entry points."""
+    # If we're running on Python 2, take responsibility for preventing
+    # output from causing UnicodeEncodeErrors. (Done here so it should only
+    # happen when not being imported by some other program.)
+    import sys
+    if sys.version_info.major < 3:
+        reload(sys)
+        sys.setdefaultencoding('utf-8')  # pylint: disable=no-member
+
+    from argparse import ArgumentParser, RawTextHelpFormatter
+    parser = ArgumentParser(formatter_class=RawTextHelpFormatter,
+            description=__doc__.replace('\r\n', '\n').split('\n--snip--\n')[0])
+    parser.add_argument('--version', action='version',
+            version="%%(prog)s v%s" % __version__)
+    parser.add_argument('-v', '--verbose', action="count",
+        default=2, help="Increase the verbosity. Use twice for extra effect")
+    parser.add_argument('-q', '--quiet', action="count",
+        default=0, help="Decrease the verbosity. Use twice for extra effect")
+    # Reminder: %(default)s can be used in help strings.
+
+    args = parser.parse_args()
+
+    # Set up clean logging to stderr
+    log_levels = [logging.CRITICAL, logging.ERROR, logging.WARNING,
+                  logging.INFO, logging.DEBUG]
+    args.verbose = min(args.verbose - args.quiet, len(log_levels) - 1)
+    args.verbose = max(args.verbose, 0)
+    logging.basicConfig(level=log_levels[args.verbose],
+                        format='%(levelname)s: %(message)s')
+
+
+
+if __name__ == '__main__':
+    main()
+
+# vim: set sw=4 sts=4 expandtab :
